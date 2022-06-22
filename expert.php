bfd0445326c35a5a80f268932ebfa7efba5ccc57
Ruben Delbrassine <ruben.delbrassine@gmail.com>
2022-06-22 09:59:26 +0200

    Complete ex. 9

--- expert.php
+++ expert.php
@@ -126,3 +126,16 @@ echo isLinkValid('http://google.com');
 echo isLinkValid('http://google.com/test.txt');
 
 
+new_exercise(9);
+// === Exercise 9 ===
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+for($i=0; $i <= count($areTheseFruits); $i++) {
+    if(!in_array($areTheseFruits[$i], $validFruits)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits);//do not change this
